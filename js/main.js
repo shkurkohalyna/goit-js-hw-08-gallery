956494229d0f34cbedb5112333a3b54e5ce548a2
shkurkohalyna <krysty1105@gmail.com>
2020-10-26 00:20:22 +0200

    gal

--- js/main.js
+++ js/main.js
@@ -1,24 +1,68 @@
 import products from './gallery-items.js';
+console.log(products);
 
 const galleryRef = document.querySelector('.js-gallery');
 const galleryItemMarkup = createGalleryItemMarkup(products);
+const lightbox = document.querySelector('.lightbox');
+const lightboxImage = document.querySelector('.lightbox__image');
+
+
 
 function createGalleryItemMarkup(products) {
-    return products.map(product => {
+  return products.map(({preview, original, description}) => {
         return `<li class="gallery__item">
   <a
     class="gallery__link"
-    href="${product.original}"
+    href="${original}"
   >
     <img
       class="gallery__image"
-      src="${product.preview}"
-      data-source="${product.original}"
-      alt="${product.description}"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
     />
   </a>
 </li>`;
     }).join('');
 };
 
-galleryRef.insertAdjacentHTML('afterbegin', galleryItemMarkup);
+
+galleryRef.insertAdjacentHTML('beforeend', galleryItemMarkup);
+galleryRef.addEventListener('click', onGaleryItemClick);
+
+
+function onGaleryItemClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  lightbox.classList.add('is-open');
+  lightboxImage.scr = event.target.dataset.source;
+  lightboxImage.alt = event.target.alt;
+}
+
+const closeIcon = document.querySelector('[data-action="close-lightbox"]');
+closeIcon.addEventListener('click', closeModal);
+
+
+
+function closeModal(event) {
+  if (event.target.nodeName === 'IMG') {
+    return;
+  }
+  lightbox.classList.remove('is-open');
+  lightboxImage.removeAttribute('src');
+  lightboxImage.removeAttribute("alt");
+}
+// function closeModal() {
+//   window.removeEventListener('keydown', onEscKeyPress);
+//   lightbox.classList.remove('is-open');
+// }
+
+// function onEscKeyPress(event) {
+//   const ESC_KEY = 'Escape';
+//   const isESCKey = event.code === ESC_KEY;
+//   if (isESCKey) {
+//     closeModal();
+// }
+// }
